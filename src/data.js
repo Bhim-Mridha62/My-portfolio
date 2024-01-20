9556e817ed1c4a6e30554d8d3a7c2c0b816c3826
Bhim Mridha <bhimmridha62@gmail.com>
2024-01-20 12:13:51 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const projects = [
+  {
+    title: "Netflix Clone",
+    subtitle: "React js",
+    description:
+      "Netflix Clone - React JS: A React-based project mimicking Netflix, enabling users to explore movies and shows within a user-friendly interface. Ideal for learning React.",
+    image: "./project-1.gif",
+    link: "https://github.com/Bhim-Mridha62/Netflix-Clone-React",
+  },
+  {
+    title: "Real time Chat app",
+    subtitle: "React js and Node js",
+    description:
+      "Real-time Chat App - React and Node JS: A MERN stack project offering real-time messaging, built with React and Node. Experience seamless communication in an engaging environment.",
+    image: "./project-2.gif",
+    link: "https://github.com/Bhim-Mridha62/MERN-chat-app",
+  },
+  {
+    title: "tourism app",
+    subtitle: "MERN",
+    description:
+      "Tourism App - MERN Stack: Explore, plan, and book your dream destinations with ease. A full-stack MERN application simplifying tourism and ticket booking. Explore on GitHub for insights.",
+    image: "./project-3.gif",
+    link: "https://github.com/Bhim-Mridha62/tourism-and-tickets",
+  },
+  {
+    title: "calculater app",
+    subtitle: "React",
+    description:
+      "React Calculator: A user-friendly calculator built using React, simplifying arithmetic operations. Offers a clean interface for efficient calculations and problem-solving on any device.",
+      image: "./project-4.gif",
+    link: "https://github.com/Bhim-Mridha62/react-calculator",
+  },
+  {
+    title: "Sopping Cart",
+    subtitle: "React,Redux and React Router Dom",
+    description:
+      "Shopping Cart: A React-based application utilizing Redux and React Router Dom for seamless management of products and cart functionalities.",
+      image: "./project-5.gif",
+    link: "https://github.com/Bhim-Mridha62/Sopping-Cart",
+  },
+];
+export const skills = [
+  "JavaScript",
+  "Html",
+  "CSS",
+  "React js",
+  "React Redux , React Router Dom",
+  "bootstrap",
+  "express js",
+  "Node js",
+  "JAVA",
+  "C",
+  "Mongodb",
+];
